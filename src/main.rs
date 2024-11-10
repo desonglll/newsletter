15448d507a7c11a6cb7ee812ee3db9071dfffadf
mikeshinoda <53910940+desonglll@users.noreply.github.com>
2024-11-10 15:47:58 +0800

    Update.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use std::net::TcpListener;
 
-use newsletter::{configuration::get_configuration, startup::run};
+use newsletter::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
+use sqlx::PgPool;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("newsletter".into(), "info".into());
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
