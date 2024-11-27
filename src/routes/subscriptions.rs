acc76fbc3770d2f704b158ea6b5c15cf7fd68b1d
desonglll <lindesong666@163.com>
2024-11-27 08:24:03 +0800

    Next is 7.7.2.3.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -29,7 +29,14 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>, email
     if insert_subscriber(&pool, &new_subscriber).await.is_err() {
         return HttpResponse::InternalServerError().finish();
     }
-    if email_client.send_email(new_subscriber.email, "Welcome!", "Welcome to our newsletter!", "Welcome to our newsletter!").await.is_err() {
+
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
+    if email_client.send_email(new_subscriber.email,
+                               "Welcome!",
+                               &format!("Welcome to our newsletter!<br />\
+    Click <a href=\"{}\">here</a> to confirm your subscription.", confirmation_link),
+                               &format!("Welcome to our newsletter!\nVisit {} to confirm your subscription.", confirmation_link)).await.is_err()
+    {
         return HttpResponse::InternalServerError().finish();
     }
     HttpResponse::Ok().finish()
