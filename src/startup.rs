f7fba23896e043d83cfbc2a75f2d69052de9c207
desonglll <lindesong666@163.com>
2024-11-27 08:40:27 +0800

    Next is 7.7.5.

--- src/startup.rs
+++ src/startup.rs
@@ -12,6 +12,8 @@ use crate::{
     email_client::EmailClient,
     routes::{health_check, subscribe},
 };
+use crate::routes::confirm;
+
 pub struct Application {
     port: u16,
     server: Server,
@@ -68,6 +70,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
     })
