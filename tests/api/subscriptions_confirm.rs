486e46c6558047e2ced68cc8a9ba21cf4d3f0aba
desonglll <lindesong666@163.com>
2024-11-27 09:39:28 +0800

    Next is 7.8.

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -18,12 +18,12 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
 
 #[tokio::test]
 async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
-    // arrange
+    // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     Mock::given(path("/email"))
-        .and(method("post"))
+        .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
         .mount(&app.email_server)
         .await;
@@ -32,10 +32,9 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
     let confirmation_links = app.get_confirmation_links(email_request);
 
-    // act
+    // Act
     let response = reqwest::get(confirmation_links.html).await.unwrap();
 
-    // assert
+    // Assert
     assert_eq!(response.status().as_u16(), 200);
 }
-
