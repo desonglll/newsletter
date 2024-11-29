34714673856717a51d9cc66ebcec936232b5dddf
desonglll <lindesong666@163.com>
2024-11-29 21:19:46 +0800

    Next is 10.6.

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,6 +2,8 @@ mod health_check;
 mod subscriptions;
 mod newsletters;
 mod subscriptions_confirm;
+mod home;
+pub use home::*;
 pub use health_check::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
--- src/startup.rs
+++ src/startup.rs
@@ -7,7 +7,7 @@ use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-use crate::routes::{confirm, publish_newsletter};
+use crate::routes::{confirm, home, publish_newsletter};
 use crate::{
     configuration::{DatabaseSettings, Settings},
     email_client::EmailClient,
@@ -77,6 +77,7 @@ pub fn run(
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .route("/", web::get().to(home))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
