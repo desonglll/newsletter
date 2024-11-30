c59483a4665c30d753f415cc4b3ee7218cd88293
desonglll <lindesong666@163.com>
2024-11-30 21:37:00 +0800

    Next is 10.6.4.3.

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,6 @@
 use actix_web::http::header::LOCATION;
 use actix_web::{web, HttpResponse, ResponseError};
+use actix_web::body::BoxBody;
 use actix_web::http::StatusCode;
 use secrecy::Secret;
 use sqlx::PgPool;
@@ -47,11 +48,18 @@ impl std::fmt::Debug for LoginError {
     }
 }
 
+
 impl ResponseError for LoginError {
     fn status_code(&self) -> StatusCode {
         match self {
             LoginError::AuthError(_) => { StatusCode::INTERNAL_SERVER_ERROR }
             LoginError::UnexpectedError(_) => { StatusCode::UNAUTHORIZED }
         }
+    }
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        let encoded_error = urlencoding::Encoded::new(self.to_string());
+        HttpResponse::build(self.status_code())
+            .insert_header((LOCATION, format!("/login?error={}", encoded_error)))
+            .finish()
     }
 }
