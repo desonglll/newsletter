c3a9712a85cfa6ae68762220fa8e000b2369b9c9
desonglll <lindesong666@163.com>
2024-12-01 10:56:11 +0800

    Next is 10.6.4.13.

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,5 +1,5 @@
 use actix_web::http::header::ContentType;
-use actix_web::{web, HttpResponse};
+use actix_web::{HttpRequest, HttpResponse};
 use hmac::{Hmac, Mac};
 use secrecy::ExposeSecret;
 use crate::startup::HmacSecret;
@@ -24,22 +24,11 @@ impl QueryParams {
     }
 }
 
-pub async fn login_form(query: Option<web::Query<QueryParams>>,
-                        secret: web::Data<HmacSecret>) -> HttpResponse {
-    let error_html = match query {
+pub async fn login_form(request: HttpRequest) -> HttpResponse {
+    let error_html = match request.cookie("_flash") {
         None => "".to_string(),
-        Some(query) => match query.0.verify(&secret) {
-            Ok(error) => {
-                format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error))
-            }
-            Err(e) => {
-                tracing::warn!(
-                    error.message = %e,
-                    error.cause_chain = ?e,
-                    "Failed to verify query parameters using the HMAC tag"
-                );
-                "".into()
-            }
+        Some(cookie) => {
+            format!("<p><i>{}</i></p>", cookie.value())
         }
     };
     HttpResponse::Ok()
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -22,4 +22,8 @@ async fn an_error_flash_message_is_set_on_failure() {
 
     let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
     assert_eq!(flash_cookie.value(), "Authentication failed");
+
+    // Act - Part 2 - Follow the redirect
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains("<p><i>Authentication failed</i></p>"));
 }
